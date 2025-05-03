08e794ff9d773b75ad670e2b53c2015df94674df
TanjiroBBBBBBBBBBBBB56 <bhempire576@gmail.com>
2025-05-04 04:56:46 +0600

    Create popunder.js

---
+++ popunder.js
@@ -0,0 +1,5 @@
+// popunder.js
+const popunderScript = document.createElement('script');
+popunderScript.type = 'text/javascript';
+popunderScript.src = '//pl26407515.profitableratecpm.com/79/80/1d/79801d82b52a071baac971d37614f753.js';
+document.body.appendChild(popunderScript);
